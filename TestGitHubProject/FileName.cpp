3c6177197be715c389d111fa3a74371c6a60da95
Student <prindov@bk.ru>
2026-04-01 16:02:15 +0200

    Изменил код

--- TestGitHubProject/FileName.cpp
+++ TestGitHubProject/FileName.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	std::count << "Hello, Git";
-		return 0;
-}
+	int arr[] = { 2, 5, 3, 5, 6, 4, 6, 9, 54, 74, 34, 5, 0 };
+	int maxEl = arr[0];
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (maxEl < arr[i])
+		{
+			maxEl = arr[i];
+		}
+	}
+
+	cout << maxEl << endl;
+	return 0;
+}	
